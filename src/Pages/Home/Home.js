f03eeba7cfb7a3da98456d5944283ddb9287f137
jahedripon9 <jahedahmed3170@gmail.com>
2023-03-04 14:42:20 +0600

    SpecialService Section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from '../HeroSection/HeroSection';
 import Partner from '../Partner/Partner';
 import Header from '../Shared/Header';
+import SpecialServices from '../SpecialServices/SpecialServices';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Header />
             <HeroSection />
             <Partner />
+            <SpecialServices />
         </div>
     );
 };
---
+++ src/Pages/SpecialServices/SpecialServices.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import doctor from '../../assets/Special.png';
+import Covid from '../../assets/covid-test 2.png';
+
+const SpecialServices = () => {
+    return (
+        <div className='container mx-auto  px-4 bg-blue-50'>
+            <div className="hero min-h-screen lg:mx-10">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+
+                    <div className='mx-auto lg:mx-24'>
+                        <h1 className="text-4xl font-bold text-black">Our <span className='py-6 text-4xl font-bold text-cyan-400'>Special Services</span></h1>
+                        <p className="py-6 lg:pr-20  text-lg font-sm text-gray-400 text-justify ">In times like today, your health is very important,
+                            especially since the number of COVID-19 cases is
+                            increasing day by day, so we are ready to help you
+                            with your health consultation</p>
+                        <div className='grid grid-cols-1 lg:w-auto lg:grid-cols-2 gap-2 '>
+                            <div className="w-56 h-60 shadow-xl ">
+                                <figure><img src={Covid} alt="Shoes" /></figure>
+                                <div className="card-body -my-10">
+                                    <h2 className="card-title">
+                                        Covid-19 Test
+                                    </h2>
+                                    <p className='text-sm text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare tellus malesuada odio blandit. Sit duis eu nisi habitant lorem egestas. </p>
+
+                                </div>
+                            </div>
+                            <div className="w-56 h-60 shadow-xl">
+                                <figure><img src={Covid} alt="Shoes" /></figure>
+                                <div className="card-body -my-10">
+                                    <h2 className="card-title">
+                                        Covid-19 Test
+                                    </h2>
+                                    <p className='text-sm text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare tellus malesuada odio blandit. Sit duis eu nisi habitant lorem egestas. </p>
+
+                                </div>
+                            </div>
+                            <div className="w-56 h-60 shadow-xl">
+                                <figure><img src={Covid} alt="Shoes" /></figure>
+                                <div className="card-body -my-10">
+                                    <h2 className="card-title">
+                                        Covid-19 Test
+                                    </h2>
+                                    <p className='text-sm text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare tellus malesuada odio blandit. Sit duis eu nisi habitant lorem egestas. </p>
+
+                                </div>
+                            </div>
+                            <div className=" w-56 h-60 shadow-xl">
+                                <figure><img className='mx-5' src={Covid} alt="Shoes" /></figure>
+                                <div className="card-body -my-10">
+                                    <h2 className="card-title">
+                                        Covid-19 Test
+                                    </h2>
+                                    <p className='text-sm text-gray-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare tellus malesuada odio blandit. Sit duis eu nisi habitant lorem egestas. </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={doctor} className="lg:w-1/2 w-2/2 rounded-lg  " />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SpecialServices;
