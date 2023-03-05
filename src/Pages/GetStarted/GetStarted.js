6ec7275cc9a2ce7c6ed0d42fe9c8f1ca817bc7e1
jahedripon9 <jahedahmed3170@gmail.com>
2023-03-05 09:42:28 +0600

    updation few section

--- src/Pages/GetStarted/GetStarted.js
+++ src/Pages/GetStarted/GetStarted.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GetStarted = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen bg-blue-200">
                 <div className="hero-content text-center">
                     <div className="max-w-md">
                         <h1 className="text-4xl font-bold">Get started with HaiDoc</h1>
